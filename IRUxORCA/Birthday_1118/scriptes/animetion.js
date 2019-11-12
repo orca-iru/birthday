7e32f6a2e3718f6892dc02dd615a7ee9bf86b72e
orca-iru <40536552+orca-iru@users.noreply.github.com>
2019-11-13 03:54:14 +0900

    Add files via upload

---
+++ IRUxORCA/Birthday_1118/scriptes/animetion.js
@@ -0,0 +1,41 @@
+
+// 以下雪のアニメーション
+anime({
+  targets: down_snow1,
+  translateY:700,
+  duration:10000,
+  autoplay: true,
+  easing: 'easeInOutSine'
+})
+
+anime({
+  targets: down_snow2,
+  translateY:700,
+  duration:10000,
+  autoplay: true,
+  easing: 'easeInOutSine'
+})
+
+anime({
+  targets: down_snow3,
+  translateY:700,
+  duration:10000,
+  autoplay: true,
+  easing: 'easeInOutSine'
+})
+
+anime({
+  targets: down_snow4,
+  translateY:700,
+  duration:10000,
+  autoplay: true,
+  easing: 'easeInOutSine'
+})
+
+anime({
+  targets: down_snow5,
+  translateY:700,
+  duration:10000,
+  autoplay: true,
+  easing: 'easeInOutSine'
+})
